8402c825feb7d87205bedf7c896fad20e0df8bdc
yogarn <radityanalaa@gmail.com>
2025-02-03 08:58:06 +0700

    `feat:` Add logger to file

--- libraries/logger/winston.ts
+++ libraries/logger/winston.ts
@@ -1,9 +1,28 @@
+import path from 'path';
 import { createLogger, format, transports } from 'winston';
+
+const customFormat = format.combine(
+  format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+  format.colorize(),
+  format.printf(({ timestamp, level, message }) => {
+    return `[${timestamp}] ${level}: ${message}`;
+  })
+);
 
 const logger = createLogger({
   level: 'info',
   format: format.json(),
-  transports: [new transports.Console()],
+  transports: [
+    new transports.Console({
+      format: customFormat,
+    }),
+
+    new transports.File({
+      filename: path.join(__dirname, '../../logs/app.log'),
+      level: 'info',
+      format: format.combine(format.timestamp(), format.json()),
+    }),
+  ],
 });
 
 export default logger;
