a456298b1cc389bd3adeaea482b42704f4a34640
yogarn <radityanalaa@gmail.com>
2025-02-15 20:11:28 +0700

    `feat:` Add pm2 autorestart if crash

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -3,6 +3,7 @@ module.exports = {
   script: 'index.ts',
   interpreter: 'bun',
   watch: true,
+  autorestart: true,
   restart_delay: 1000,
   env: {
     PATH: `${process.env.HOME}/.bun/bin:${process.env.PATH}`,
